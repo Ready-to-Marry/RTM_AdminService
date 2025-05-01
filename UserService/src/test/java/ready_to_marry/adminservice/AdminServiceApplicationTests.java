7d1f5349cdbd1f7001c65c78fbbefa519dd17428
Hyundoo <114637614+HyunDooBoo@users.noreply.github.com>
2025-05-01 18:02:22 +0900

    refactor: user -> admin 수정

--- UserService/src/test/java/ready_to_marry/adminservice/AdminServiceApplicationTests.java
+++ UserService/src/test/java/ready_to_marry/adminservice/AdminServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package ready_to_marry.userservice;
+package ready_to_marry.adminservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class UserServiceApplicationTests {
+class AdminServiceApplicationTests {
 
     @Test
     void contextLoads() {
