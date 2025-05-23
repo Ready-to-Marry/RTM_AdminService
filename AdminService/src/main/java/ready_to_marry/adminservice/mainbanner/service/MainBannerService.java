269a506634e2e3b4a20a2a8ca86dd9ab7454bb18
김리영 <fldud6522@gmail.com>
2025-05-23 10:49:28 +0900

    refactor: MainBannerService 중복 메서드 제거 및 번호 주석 추가
    - adminId가 없는 register/update/delete 메서드 제거

--- AdminService/src/main/java/ready_to_marry/adminservice/mainbanner/service/MainBannerService.java
+++ AdminService/src/main/java/ready_to_marry/adminservice/mainbanner/service/MainBannerService.java
@@ -1,24 +1,21 @@
 package ready_to_marry.adminservice.mainbanner.service;
 
-import jakarta.transaction.Transactional;
 import ready_to_marry.adminservice.mainbanner.dto.request.MainBannerRequest;
 import ready_to_marry.adminservice.mainbanner.dto.response.MainBannerResponse;
 
 import java.util.List;
 
 public interface MainBannerService {
+
+    // 1. 메인 배너 등록
     void register(MainBannerRequest request, Long adminId);
+
+    // 2. 메인 배너 수정
     void update(Long id, MainBannerRequest request, Long adminId);
+
+    // 3. 메인 배너 삭제
     void delete(Long id, Long adminId);
 
-    @Transactional
-    void register(MainBannerRequest request);
-
-    @Transactional
-    void update(Long mainBannerId, MainBannerRequest request);
-
-    @Transactional
-    void delete(Long id);
-
+    // 4. 메인 배너 전체 조회
     List<MainBannerResponse> getAll();
 }
